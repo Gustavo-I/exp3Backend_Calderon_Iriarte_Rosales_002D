56534e693da9e2920e700846248e4b315fef17cb
Msn-Challa <benj.rosales@duocuc.cl>
2021-05-15 19:13:02 -0400

    Se agrego evento modo oscuro

--- js/funciones.js
+++ js/funciones.js
@@ -3,14 +3,12 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
+    if (document.body.scrollTop > 100){
+      mybutton.style.transform="scale(1)";
     }
-  }
+}
 
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
+}

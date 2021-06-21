a80d674f86f157870ebb6ea8292e13be911ba39b
Msn-Challa <benj.rosales@duocuc.cl>
2021-06-21 18:47:39 -0400

    Se arreglo Index

--- TestDjango/valhalla/views.py
+++ TestDjango/valhalla/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html',)
+    return render(request, 'index.html')
 def tienda(request):
-    return render(request, 'tienda.html',)
+    return render(request, 'tienda.html')

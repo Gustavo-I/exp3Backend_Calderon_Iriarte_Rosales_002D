a3d683a0ea750346ab7242aa6e07188940fa3084
Msn-Challa <benj.rosales@duocuc.cl>
2021-06-20 20:49:24 -0400

    Se arreglaron unos problemas

--- TestDjango/TestDjango/settings.py
+++ TestDjango/TestDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'valhalla',
+    'valhalla.apps.ValhallaConfig',
 ]
 
 MIDDLEWARE = [
